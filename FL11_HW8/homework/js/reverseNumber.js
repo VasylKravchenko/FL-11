1cae4e32b760f0a797e1112fb4ef342d994bd297
VasylKravchenko <vasylkravchenko@gmail.com>
2019-07-12 19:57:49 +0300

    reverseNumber

---
+++ FL11_HW8/homework/js/reverseNumber.js
@@ -0,0 +1,14 @@
+function reverseNumber(num){
+    let result = 0;
+    let digit;
+    while (num !== 0) {
+        digit = num % 10;
+        result = (result * 10) + digit;
+        num = (num - digit) / 10;
+        
+    }
+    return result;
+ }
+console.log(reverseNumber(398));//893
+console.log(reverseNumber(-746));//-647
+console.log(reverseNumber(30000));//3
