fd3122904a3b6e0eebda3e00e7aa6a133c0c7255
VasylKravchenko <vasylkravchenko@gmail.com>
2019-07-13 15:40:46 +0300

    t0 getNumbers

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+//task0
+function getNumbers(str) {
+    let numsArr = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] % 1 === 0) {
+            numsArr.push(+str[i]);
+        }
+    }
+    return numsArr;
+}
+
+console.log(getNumbers('djf23ju34-2j3'));
