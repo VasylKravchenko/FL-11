b75cd292096b496699de676fb4792ff427abbf2d
VasylKravchenko <vasylkravchenko@gmail.com>
2019-07-14 22:52:20 +0300

    t1 findTypes

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -10,3 +10,20 @@ function getNumbers(str) {
 }
 
 console.log(getNumbers('djf23ju34-2j3'));
+
+//task1
+function findTypes() {
+    let typeStore = {};
+    for (let i of arguments) {
+        if (typeStore.hasOwnProperty(typeof i)) {
+            typeStore[typeof i] += 1;
+        } else {
+            typeStore[typeof i] = 1;
+        }
+    }
+    return typeStore;
+}
+
+console.log(findTypes(null, true, 0, NaN, false, 'hello'));
+
+//task2
