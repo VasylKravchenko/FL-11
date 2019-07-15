df4e754bdf505f26f3a0bd29a4418feff5934349
VasylKravchenko <vasylkravchenko@gmail.com>
2019-07-15 19:17:58 +0300

    t8 getAmountOfAdultPeople

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -91,3 +91,51 @@ function daysBetween(date1, date2) {
 }
 
 console.log(daysBetween(new Date('2016-03-18T00:00:00'), new Date('2016-04-19T00:00:00')));
+
+//task8
+function getAmountOfAdultPeople(data) {
+    let today = new Date();
+    const daysPerYear = 365;
+    const ageAdult = 18;
+    return filterArray(data, function (data) {
+        let bDay = new Date(data.birthday);
+        return daysBetween(bDay, today) / daysPerYear > ageAdult
+    }).length;
+}
+
+let inputData = [
+    {
+      '_id': '5b5e3168c6bf40f2c1235cd6',
+      'index': 0,
+      'birthday': '2016-03-18T00:00:00',
+      'eyeColor': 'green',
+      'name': 'Stein',
+      'favoriteFruit': 'apple'
+    },
+    {
+      '_id': '5b5e3168e328c0d72e4f27d8',
+      'index': 1,
+      'birthday': '1991-02-11T00:00:00',
+      'eyeColor': 'blue',
+      'name': 'Cortez',
+      'favoriteFruit': 'strawberry'
+    },
+    {
+      '_id': '5b5e3168cc79132b631c666a',
+      'index': 2,
+      'birthday': '1984-04-17T00:00:00',
+      'eyeColor': 'blue',
+      'name': 'Suzette',
+      'favoriteFruit': 'apple'
+    },
+    {
+      '_id': '5b5e31682093adcc6cd0dde5',
+      'index': 3,
+      'birthday': '1994-04-17T00:00:00',
+      'eyeColor': 'green',
+      'name': 'George',
+      'favoriteFruit': 'banana'
+    }
+];
+
+console.log(getAmountOfAdultPeople(inputData));
